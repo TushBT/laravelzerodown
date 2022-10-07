346290a43a0354f7af2db5e8c503bd610c52434a
TushBT <thetushbt@gmail.com>
2022-10-07 17:02:22 +0530

    update deploy file

--- deploy.php
+++ deploy.php
@@ -6,13 +6,12 @@ require 'recipe/laravel.php';
 // Config
 
 set('repository', 'https://github.com/TushBT/laravelzerodown.git');
-
+set ('ssh_multiplexing', false);
 // Hosts
 
 host('13.233.54.179')
     ->set('remote_user', 'mylaravel')
-    ->set('deploy_path', '/home/mylaravel/htdocs/www.livealoo.com')
-    ->set('ssh_multiplexing', true);
+    ->set('deploy_path', '/home/mylaravel/htdocs/www.livealoo.com');
 
     task('deploy', [
         'deploy:prepare',
